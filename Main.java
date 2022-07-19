00bca0cb54de69d288b63548cae3861dedc1e7b0
Koki <kgoudy64@gmail.com>
2022-07-19 14:32:04 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int temp = 27;
+		if (temp > 30) {
+			System.out.println("It's a hot day");
+			System.out.println("Drink water");
+		} else if (temp > 20 && temp < 30) {
+			System.out.println("It's a very nice day");
+		} else {
+			System.out.println("It's cold today.");
+			System.out.println("Dress warm");
+		}
+
+	}
+
+}
